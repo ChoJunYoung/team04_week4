c3ca80d6f26c432603c6abf464ce0e8040133d07
ChoJunYoung <jjy891224@gmail.com>
2016-04-10 14:45:18 +0900

    test

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -1,13 +1,18 @@
 package edu.team04.bill;
 
+
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest{
+	@Test
 	public void test(){
+		double total;
 		Calculator c = new Calculator(4,1000, "silver");
+		total = c.calculateTotalRate();
+	
 	}
 	
 }
