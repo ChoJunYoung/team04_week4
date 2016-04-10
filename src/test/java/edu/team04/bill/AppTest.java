04b045ac1b154c49c62d87c093bf52d6c9896182
snkim88 <snkim@cs-cnu.org>
2016-04-10 14:06:22 +0900

    initial test

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -1,34 +1,17 @@
 package edu.team04.bill;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName ) {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite(){
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp(){
-        assertTrue( true );
+public class AppTest {
+	private GoldPlan tGoldPlan;
+    
+    @Test
+    public void testGoldPlan() {
+    	tGoldPlan = new GoldPlan();
+    	Assert.assertEquals(1000, tGoldPlan.getIncludedMinute());
     }
 }
