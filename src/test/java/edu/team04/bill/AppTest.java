39697fa7ecf8c9097d5cb198c2b9793fedcc5a48
jsunwoo <jsunwoo@ualberta.ca>
2016-04-10 16:22:40 +0900

    dff

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -73,4 +73,27 @@ public class AppTest {
 
 		assertEquals(total, 94.29, 0.05);
 	}
+	
+	private GoldPlan tGoldPlan;
+	private SilverPlan tSilverPlan;
+
+	@Test
+	public void testGoldPlan() {
+		tGoldPlan = new GoldPlan();
+		assertEquals(1000, tGoldPlan.getIncludedMinute());
+		assertEquals(49.95, tGoldPlan.getBasicMonthlyRate(), 0);
+		assertEquals(0.45, tGoldPlan.getExcessMinuteRate(), 0);
+		assertEquals(14.5, tGoldPlan.getAdditionalLineRate(), 0);
+		assertEquals(5, tGoldPlan.getFamilyDiscountRate(), 0);
+	}
+
+	@Test
+	public void testSilverPlan() {
+		tSilverPlan = new SilverPlan();
+		assertEquals(29.95, tSilverPlan.getBasicMonthlyRate(), 0);
+		assertEquals(500, tSilverPlan.getIncludedMinute(), 0);
+		assertEquals(21.50, tSilverPlan.getAdditionalLineRate(), 0);
+		assertEquals(0.54, tSilverPlan.getExcessMinuteRate(), 0);
+		assertEquals(5, tSilverPlan.getFamilyDiscountRate(), 0);
+	}
 }
