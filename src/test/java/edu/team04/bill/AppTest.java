8817cc8b2eec80b61b57b2c4e3282d7842473a78
jsunwoo <jsunwoo@ualberta.ca>
2016-04-10 15:24:23 +0900

    C

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -1,34 +1,76 @@
 package edu.team04.bill;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName ) {
-        super( testName );
-    }
+public class AppTest {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	@Test
+	public void testEx1True() {
+		double total;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite(){
-        return new TestSuite( AppTest.class );
-    }
+		Calculator Ex = new Calculator(4, 878, "Gold");
+		total = Ex.calculateTotalRate();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp(){
-        assertTrue( true );
-    }
+		assertEquals(total, 83.95, 0.05);
+	}
+
+	@Test
+	public void testEx2True() {
+		double total;
+
+		Calculator Ex = new Calculator(1, 1123, "gold");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 105.3, 0.05);
+	}
+
+	@Test
+	public void testEx3True() {
+		double total;
+
+		Calculator Ex = new Calculator(4, 1123, "Gold");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 139.3, 0.05);
+	}
+
+	@Test
+	public void testEx4True() {
+		double total;
+
+		Calculator Ex = new Calculator(2, 523, "silver");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 63.87, 0.05);
+	}
+
+	@Test
+	public void testEx5True() {
+		double total;
+
+		Calculator Ex = new Calculator(5, 44, "silver");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 82.95, 0.05);
+	}
+
+	@Test
+	public void testEx6True() {
+		double total;
+
+		Calculator Ex = new Calculator(5, 521, "silver");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 94.29, 0.05);
+	}
 }
