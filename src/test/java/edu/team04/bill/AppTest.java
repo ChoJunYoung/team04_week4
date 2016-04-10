bb79927e33d92c9cce9ba3b6b74d362d98e93ead
snkim88 <snkim@cs-cnu.org>
2016-04-10 15:14:44 +0900

    additional pom.xml edits goldplan test case updates

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -9,9 +9,13 @@ import org.junit.Test;
 public class AppTest {
 	private GoldPlan tGoldPlan;
     
-    @Test
+	@Test
     public void testGoldPlan() {
     	tGoldPlan = new GoldPlan();
     	Assert.assertEquals(1000, tGoldPlan.getIncludedMinute());
+    	Assert.assertEquals(49.95, tGoldPlan.getBasicMonthlyRate(), 0);
+    	Assert.assertEquals(0.45, tGoldPlan.getExcessMinuteRate(), 0);
+    	Assert.assertEquals(14.5, tGoldPlan.getAdditionalLineRate(), 0);
+    	Assert.assertEquals(5, tGoldPlan.getFamilyDiscountRate(), 0);
     }
 }
