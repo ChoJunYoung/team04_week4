bc77bcc7d5990883766457f1fdd0fe3a3da82a72
ChoJunYoung <jjy891224@gmail.com>
2016-04-10 13:56:10 +0900

    reset

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -1,11 +1,34 @@
 package edu.team04.bill;
 
-import org.junit.Test;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName ) {
+        super( testName );
+    }
 
-public class AppTest{
-	@Test
-	public void calculatortest(){
-		
-	}
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite(){
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp(){
+        assertTrue( true );
+    }
 }
