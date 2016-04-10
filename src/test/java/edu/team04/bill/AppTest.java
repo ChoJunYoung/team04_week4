cfb7c4b32855853fbb8832628312a2f70621ad74
jsunwoo <jsunwoo@ualberta.ca>
2016-04-10 17:56:53 +0900

    asdff

--- src/test/java/edu/team04/bill/AppTest.java
+++ src/test/java/edu/team04/bill/AppTest.java
@@ -74,27 +74,63 @@ public class AppTest {
 		assertEquals(total, 94.29, 0.05);
 	}
 	
-	private GoldPlan tGoldPlan;
-	private SilverPlan tSilverPlan;
-
 	@Test
-	public void testGoldPlan() {
-		tGoldPlan = new GoldPlan();
-		assertEquals(1000, tGoldPlan.getIncludedMinute());
-		assertEquals(49.95, tGoldPlan.getBasicMonthlyRate(), 0);
-		assertEquals(0.45, tGoldPlan.getExcessMinuteRate(), 0);
-		assertEquals(14.5, tGoldPlan.getAdditionalLineRate(), 0);
-		assertEquals(5, tGoldPlan.getFamilyDiscountRate(), 0);
+	public void testEx7True() {
+		double total;
+
+		Calculator Ex = new Calculator(2, 1010, "gold");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 68.95, 0.05);	
 	}
-
+	
 	@Test
-	public void testSilverPlan() {
-		tSilverPlan = new SilverPlan();
-		assertEquals(29.95, tSilverPlan.getBasicMonthlyRate(), 0);
-		assertEquals(500, tSilverPlan.getIncludedMinute(), 0);
-		assertEquals(21.50, tSilverPlan.getAdditionalLineRate(), 0);
-		assertEquals(0.54, tSilverPlan.getExcessMinuteRate(), 0);
-		assertEquals(5, tSilverPlan.getFamilyDiscountRate(), 0);
+	public void testEx8True() {
+		double total;
+
+		Calculator Ex = new Calculator(1, 700, "gold");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 49.95, 0.05);
+	}
+	
+	@Test
+	public void testEx9True() {
+		double total;
+
+		Calculator Ex = new Calculator(2, 521, "gold");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 64.45, 0.05);
+	}
+	
+	@Test
+	public void testEx10True() {
+		double total;
+
+		Calculator Ex = new Calculator(2, 531, "silver");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 68.19, 0.05);
+	}
+	
+	@Test
+	public void testEx11True() {
+		double total;
+
+		Calculator Ex = new Calculator(1, 400, "silver");
+		total = Ex.calculateTotalRate();
+
+		assertEquals(total, 29.95, 0.05);
+	}
+	
+	@Test
+	public void testEx12True() {
+		double total;
+
+		Calculator Ex = new Calculator(3, 400, "silver");
+		total = Ex.calculateTotalRate();
+		assertEquals(total, 72.95, 0.05);
 	}
 	
 	@Test
