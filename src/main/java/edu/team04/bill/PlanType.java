86d20aefc440986dbc877f2829e30cb4f0699f17
ChoJunYoung <jjy891224@gmail.com>
2016-04-27 17:54:13 +0900

    test

--- src/main/java/edu/team04/bill/PlanType.java
+++ src/main/java/edu/team04/bill/PlanType.java
@@ -1,10 +1,14 @@
 package edu.team04.bill;
 
 interface PlanType{
-	public double getBasicMonthlyRate();
-	public double getExcessMinuteRate();
-	public int getIncludedMinute();
-	public double getFamilyDiscountRate();
-	public double getAdditionalLineRate();
+	double getBasicMonthlyRate();
+
+	double getExcessMinuteRate();
+
+	int getIncludedMinute();
+
+	double getFamilyDiscountRate();
+
+	double getAdditionalLineRate();
 	
 }
